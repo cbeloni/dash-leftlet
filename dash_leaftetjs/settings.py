9ffa43aaf10d5bb4791cd9e71a071e0b5a9849c7
Cauê Beloni <cbeloni@gmail.com>
2023-04-07 11:37:23 -0300

    add .env

--- dash_leaftetjs/settings.py
+++ dash_leaftetjs/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8wrpux$%6+@m*ej%u$i!+)qc_s4_f3r72w)&%aldopzth3fzg1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

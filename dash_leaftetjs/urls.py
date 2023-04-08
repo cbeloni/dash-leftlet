baa7e095c34fd404211470226d1b77ad0ac9381d
Cauê Beloni <cbeloni@gmail.com>
2023-04-07 21:12:22 -0300

    adicionado menu e bootstrap

--- dash_leaftetjs/urls.py
+++ dash_leaftetjs/urls.py
@@ -20,5 +20,6 @@ from leaftlet import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path("dashboard", views.dash, name="dash")
 ]
--- leaftlet/views.py
+++ leaftlet/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def dash(request):
+    return render(request, 'dash.html')

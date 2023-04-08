289a5073d85100fe2c5af21a88e9340b4e258c68
Cauê Beloni <cbeloni@gmail.com>
2023-04-07 21:28:32 -0300

    adiconado dashboard

---
+++ leaftlet/qualidade_ar.py
@@ -0,0 +1,70 @@
+import requests
+from cachetools import cached, LRUCache, TTLCache
+import json
+from datetime import datetime
+class QualidadeAr:
+    def __init__(self, nome, situacao_rede, tipo_rede, data, qualidade):
+        self.nome = nome
+        self.situacao_rede = situacao_rede
+        self.tipo_rede = tipo_rede
+        self.data = data
+        self.qualidade = qualidade
+class Detalhes:
+    def __init__(self, nome, indice, data):
+        self.nome = nome
+        self.indice = indice
+        self.data = data
+
+def listar_todos():
+    lista_qualidade_ar2 = _get_cetesb()
+    return lista_qualidade_ar2
+
+def listar_detalhes():
+    return _listar_detalhes()
+
+@cached(cache=TTLCache(maxsize=1024, ttl=3600))
+def _get_cetesb():
+    url = "https://arcgis.cetesb.sp.gov.br/server/rest/services/QUALAR/CETESB_QUALAR/MapServer/6/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry={%22xmin%22:-5244191.63658331,%22ymin%22:-2739503.0937498696,%22xmax%22:-5165920.11961926,%22ymax%22:-2661231.5767858177,%22spatialReference%22:{%22wkid%22:102100}}&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100"
+    response = requests.get(url)
+    data = response.json()
+    lista_qualidade_ar = []
+    for feature in data['features']:
+        nome = feature['attributes']['Nome']
+        data = feature['attributes']['DATA'][:16]
+        situacao_rede = feature['attributes']['Situacao_Rede']
+        qualidade = feature['attributes']['Qualidade'] or 'Não coletado'
+        tipo_rede = feature['attributes']['Tipo_Rede']
+        lista_qualidade_ar.append(QualidadeAr(nome=nome, situacao_rede=situacao_rede, tipo_rede=tipo_rede, data=data, qualidade=qualidade))
+
+
+    return lista_qualidade_ar
+
+def _listar_detalhes():
+    url = "https://arcgis.cetesb.sp.gov.br/server/rest/services/QUALAR/CETESB_QUALAR/MapServer/0/query?f=json&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*"
+    #json_data = '{"M1": 2, "TM1": 1678550400000, "M2": 2, "TM2": 1678546800000, "M3": 2, "TM3": 1678543200000, "M4": 2, "TM4": 1678539600000, "M5": 2, "TM5": 1678536000000}'
+    response = requests.get(url)
+    features = json.loads(response.content)['features']
+
+    lista_completa_detalhes = []
+    for f in features:
+        print (f['attributes']['ID'])
+        print(f['attributes']['STATNM'])
+        nome = f['attributes']['STATNM']
+        del f['attributes']['ID']
+        del f['attributes']['STATNM']
+        detalhes = _atribute_detalhes(f['attributes'], nome)
+        lista_completa_detalhes.append(detalhes)
+    return lista_completa_detalhes
+
+def _atribute_detalhes(data, nome):
+    lista_detalhes = []
+    keys = list(data.keys())
+    count = 24 if len(keys) > 24 else len(keys)
+    for i in range(count, 0, -2):
+        valueIndice = data[keys[i]]
+        valueData = datetime.fromtimestamp(data[keys[i + 1]] / 1000).strftime("%Y-%m-%d %Hh")
+        lista_detalhes.append(Detalhes(indice=valueIndice, data=valueData, nome=nome))
+    return lista_detalhes
+
+if __name__ == '__main__':
+    detalhes = listar_detalhes()
--- leaftlet/views.py
+++ leaftlet/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .qualidade_ar import listar_todos, listar_detalhes
 
 def home(request):
     return render(request, 'home.html')
 
 def dash(request):
-    return render(request, 'dash.html')
+    detalhes = listar_detalhes()
+    # Page from the theme
+    return render(request, 'dash.html',{ "lista_qualidade_ar": listar_todos(),
+                                                 "detalhes": detalhes})

77d29485e50ea4ea4c6616dd237172b50889d2a5
Cauê Beloni <cbeloni@gmail.com>
2023-04-10 22:39:36 -0300

    ajuste out of range

--- leaftlet/views.py
+++ leaftlet/views.py
@@ -11,7 +11,7 @@ def home(request):
     for feature in grande_sp_indices['features']:
         if feature['properties']['Indice'] is not None:
             matching_items = [item for item in lista_qualidade_ar if item.municipio.lower() == feature['properties']['Municipio'].lower() and item.indice is not None]
-            feature['properties']['Indice'] = matching_items[0].indice
+            feature['properties']['Indice'] = matching_items[0].indice if any(matching_items) else None
 
     return render(request, 'home.html', { "grande_sp_indices": json.dumps(grande_sp_indices)})
 

736c55e64fee1c477ac94d6000e6a53b00494cfd
Cauê Beloni <cbeloni@gmail.com>
2023-04-23 18:09:46 -0300

    add teste do models

--- leaftlet/models.py
+++ leaftlet/models.py
@@ -1,21 +1,20 @@
 from django.db import models
+from datetime import date
 
 
 class QualidadeAr(models.Model):
     class Meta:
         db_table = 'qualidade_ar'
 
-    title = models.CharField(max_length=200)
-    seconds = models.IntegerField()
     nome = models.CharField(max_length=255)
     situacao_rede = models.CharField(max_length=10)
     tipo_rede = models.CharField(max_length=5)
-    data = models.DateField()
+    data = models.DateField(default=date.today)
     qualidade = models.CharField(max_length=255)
-    endereco = models.CharField(max_length=255)
-    indice = models.IntegerField()
+    endereco = models.CharField(max_length=255, null=True)
+    indice = models.IntegerField(default=0)
     poluente = models.CharField(max_length=10)
-    municipio = models.CharField(max_length=255)
+    municipio = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.title
--- leaftlet/test/test_models.py
+++ leaftlet/test/test_models.py
@@ -6,5 +6,5 @@ pytestmark = pytest.mark.django_db
 
 class TestQualidadeAr:
     def test_qualidade_ar(self):
-        my_model = mixer.blend("leaftlet.QualidadeAr")
-        assert my_model.pk == 1, "Should create a MyModel instance"
+        qualidade_ar = mixer.blend("leaftlet.QualidadeAr")
+        assert qualidade_ar.pk == 1, "Should create a QualidadeAr instance"

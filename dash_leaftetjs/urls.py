035926b682694dcffc47071607c18e1256049bb1
Cauê Beloni <cbeloni@gmail.com>
2023-05-05 23:42:27 -0300

    add proxy param

--- dash_leaftetjs/urls.py
+++ dash_leaftetjs/urls.py
@@ -21,5 +21,7 @@ from leaftlet import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home, name="home"),
-    path("dashboard", views.dash, name="dash")
+    path('datasaude', views.home, name="home"),
+    path("dashboard", views.dash, name="dash"),
+    path("dashboard/dashboard", views.dash, name="dash")
 ]
